21a416564c676dda05713b3168ba774d7ec45fb5
zhangru <2495760174@qq.com>
2023-09-25 16:13:28 +0800

    完成项目后端初始化模板

---
+++ inkAPI-backend/src/main/java/com/inkslab/springbootinit/service/InterfaceInfoService.java
@@ -0,0 +1,13 @@
+package com.inkslab.springbootinit.service;
+
+import com.inkslab.springbootinit.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 24957
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2023-09-25 16:09:29
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+}
