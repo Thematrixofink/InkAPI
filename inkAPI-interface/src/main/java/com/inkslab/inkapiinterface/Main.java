7ed64a5fe4125eb8d32a5710eb39dcda4701e299
zhangru <2495760174@qq.com>
2023-10-16 16:44:18 +0800

    跑通业务流程

--- inkAPI-interface/src/main/java/com/inkslab/inkapiinterface/Main.java
+++
@@ -1,9 +0,0 @@
-package com.inkslab.inkapiinterface;
-
-
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- inkAPI-interface/src/main/java/com/inkslab/inkapiinterface/controller/NameController.java
+++ inkAPI-interface/src/main/java/com/inkslab/inkapiinterface/controller/NameController.java
@@ -23,22 +23,6 @@ public class NameController {
 
     @PostMapping("/post/body")
     public String getNameByBody(@RequestBody User user, HttpServletRequest request){
-        String accessKey = request.getHeader("accessKey");
-        String nonce = request.getHeader("nonce");
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
-        String body = request.getHeader("body");
-
-        //todo 去数据库里查询accesskey，是否存在
-        if(!accessKey.equals("accessKey")){
-            throw  new RuntimeException("无权限");
-        }
-        //服务端的secretKey是分发给用户的，是知道的
-        //todo secretKey也是从服务器里查的
-        String serverSign = SignUtil.getSign(body, "secretKey");
-        if(!sign.equals(serverSign)){
-            throw new RuntimeException("无权限");
-        }
         String result = "POST BODY 你的名字是:"+user.getName();
         return result;
     }
